3234a303d8398dc972da867073132f9306695687
Ro-shines-meesho <nekkanti.roshni@meesho.com>
2025-08-04 15:11:01 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Aushadi"> {/* 👈 Add this */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
